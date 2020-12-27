914ed919e3450e9a7d5ef9ae4d09a2bc34240b66
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2020-12-28 04:44:43 +0500

    updated scripts

---
+++ sqlscripts/populate.sql
@@ -0,0 +1,20 @@
+insert into status values(1,1,"zain@abc.com",NULL,"Underground honey ka time hai","2020-12-12",22,11,"AJK");
+insert into status values(2,2,"zain@abc.com",NULL,"Sunshine","2020-11-12",22,11,"AJK");
+insert into status values(3,3,"zain@abc.com",NULL,"PAKISTAN ZINDABAD","2020-11-12",22,11,"AJK");
+insert into status values(4,4,"zain@abc.com",NULL,"I love my country","2020-11-12",22,11,"AJK");
+insert into status values(5,5,"fatima@abc.com",NULL,"PAKISTAN ZINDABAD YEAH","2020-11-12",22,11,"AJK");
+insert into status values(6,6,"maaz@abc.com",NULL,"I love my country USA","2020-11-12",22,11,"AJK");
+
+
+Insert into page values("ibm@abc.com","random",1,"Finance","IBM PAKISTAN","GREP PPL","NY","Arizona",36);
+Insert into page values("bentley@abc.com","random",4,"SE","BENTLEY PAKISTAN","GREP PPL","NY","Arizona",36);
+Insert into page values("systems@abc.com","random",6,"CS","SYSTEMS PAKISTAN","GREP PPL","NY","Arizona",36);
+
+
+insert into profile_follows_page values("ibm@abc.com","zain@abc.com");
+
+insert into profile_follows_profile values("zain@abc.com","anas@abc.com");
+insert into profile_follows_profile values("zain@abc.com","fatima@abc.com");
+
+
+
